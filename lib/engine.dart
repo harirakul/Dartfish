9eb75ebc46efba6828a3d43b6ccb2515299b0d74
harirakul <harirakul.a@gmail.com>
2020-12-26 10:29:43 -0800

    Added a check for draws

--- lib/engine.dart
+++ lib/engine.dart
@@ -1,18 +1,6 @@
 import 'dart:math';
-
 import "package:chess/chess.dart";
-
-var turns = <String, int>{"w": 1, "b": -1};
-var material_values = <String, int>{
-  "p": 100,
-  "n": 320,
-  "b": 330,
-  "r": 500,
-  "q": 900,
-  "k": 10000
-};
-
-double inf = 100000;
+import 'consts.dart';
 
 List minimax(String position, int depth, bool maxPlayer) {
   if (depth == 0 || Chess.fromFEN(position).in_checkmate) {
@@ -53,8 +41,11 @@ class Engine extends Chess {
   Engine.fromFEN(String fen) : super.fromFEN(fen);
 
   double eval() {
-    // Basic Material Evaluation
+    if (in_stalemate || insufficient_material || in_threefold_repetition) {
+      return 0;
+    }
 
+    // Basic Material Evaluation
     double score = 0;
     for (int i = 0; i < board.length; i++) {
       if (board[i] is Piece) {
