92bcee63824edf618424ad6df14d0a3d439c6107
harirakul <harirakul.a@gmail.com>
2020-12-25 17:08:35 -0800

    Added UCI Protocol handler

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,26 @@
+import 'dart:io';
 import "engine.dart";
 
+var commands = <String, List>{
+  "uci": ["id name Dartfish", "id author Hari Ambethkar", "uciok"],
+  "isready": ["readyok"],
+};
+
+void run() {
+  Engine bot = new Engine();
+  while (true) {
+    String cmd = stdin.readLineSync();
+    if (commands.containsKey(cmd)) {
+      for (String out in commands[cmd]) {
+        print(out);
+      }
+    } 
+    else if (cmd == "go") {
+      print("bestmove " + bot.play()[1]);
+    }
+  }
+}
+
 void main() {
-  Engine e = new Engine.fromFEN('1rr1k3/4p1b1/6Q1/q2Pp3/4P3/2p2PN1/P3N1K1/R6R b - - 0 31');
-  print(e.eval());
+  run();
 }
